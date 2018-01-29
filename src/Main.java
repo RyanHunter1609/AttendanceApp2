d707c55931194f449f0c330b25943b22f77e26a4
Ryan Hunter <rhunter4@xula.edu>
2018-01-29 01:54:43 -0600

    Create and array list and then output the list of absences.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,33 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Read the user's [name] from stdin and output it as the app's welcome message.
         System.out.println("Welcome to Attendance App!");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Your Name: ");
         String userName = scanner.nextLine().toUpperCase();
         System.out.println("Welcome " + userName);
+
+        ArrayList absences = listOfAbsences(userName);
+        //output the list of absences.
+        System.out.println("The Elements Are: " + absences);
+
+    }
+
+
+    private static ArrayList<Integer> listOfAbsences(String userName) {
+
+        //Create and array list of absences
+        ArrayList<Integer> absencesList = new ArrayList<>(userName.length());
+        Random random = new Random();
+        for (int i = 0; i < userName.length(); i++) {
+            absencesList.add(random.nextInt(10) - 1);
+        }
+        return absencesList;
     }
 }
+
