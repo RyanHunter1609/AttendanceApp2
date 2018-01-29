c3d2afafafdfc2686d1794e6c2fa25781020b74a
Ryan Hunter <rhunter4@xula.edu>
2018-01-28 23:50:11 -0600

    Read the user's [name] from stdin and output it as the app's welcome message.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("Welcome to Attendance App!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Name: ");
+        String userName = scanner.nextLine().toUpperCase();
+        System.out.println("Welcome " + userName);
     }
 }
